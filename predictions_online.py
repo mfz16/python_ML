17930b01e40c9e393662847abc2b2a8e73ed914c
faraz <mfz16>
2016-12-24 13:47:54 +0530

    update

---
+++ predictions_online.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from pandas import Series,DataFrame
+
+# numpy, matplotlib, seaborn
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+#%matplotlib inline
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+
+# get titanic & test csv files as a DataFrame
+titanic_df = pd.read_csv("D:/datasets/titanic/train.csv")
+test_df = pd.read_csv("D:/datasets/titanic/test.csv")
+titanic_df = titanic_df.drop(['PassengerId','Name','Ticket'], axis=1)
+test_df    = test_df.drop(['Name','Ticket'], axis=1)
+titanic_df.drop(['Embarked'], axis=1,inplace=True)
+test_df.drop(['Embarked'], axis=1,inplace=True)
+test_df["Fare"].fillna(test_df["Fare"].median(), inplace=True)
+#titanic_df['Age'].dropna().astype(int).hist(bins=70, ax=axis1)
+# test_df['Age'].dropna().astype(int).hist(bins=70, ax=axis1)
+
+# fill NaN values in Age column with random values generated
+titanic_df["Age"].fillna(titanic_df["Age"].mean(),inplace=True)
+test_df["Age"].fillna(test_df["Age"].mean(),inplace=True)
+
+titanic_df.drop(['Pclass'],axis=1,inplace=True)
+test_df.drop(['Pclass'],axis=1,inplace=True)
+
+#titanic_df.drop(['Sex'],axis=1,inplace=True)
+#test_df.drop(['Sex'],axis=1,inplace=True)
+
+titanic_df.drop(['Cabin'],axis=1,inplace=True)
+test_df.drop(['Cabin'],axis=1,inplace=True)
+
+titanic_df['Sex'] = titanic_df['Sex'].map({'female': 1, 'male': 0})
+test_df['Sex'] = test_df['Sex'].map({'female': 1, 'male': 0})
+
+X_train = titanic_df.drop("Survived",axis=1)
+Y_train = titanic_df["Survived"]
+X_test  = test_df.drop("PassengerId",axis=1).copy()
+
+# Logistic Regression
+#print(test_df.head())
+#print(X_test.head())
+logreg = LogisticRegression()
+
+logreg.fit(X_train, Y_train)
+
+Y_pred = logreg.predict(X_test)
+
+print(logreg.score(X_train, Y_train))
+#print(X_test)
+
+# Random Forests
+
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, Y_train)
+Y_pred = random_forest.predict(X_test)
+print(random_forest.score(X_train, Y_train))
+
+#knn
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, Y_train)
+Y_pred = knn.predict(X_test)
+print(knn.score(X_train, Y_train))
+
+# Gaussian Naive Bayes
+
+gaussian = GaussianNB()
+
+gaussian.fit(X_train, Y_train)
+
+Y_pred = gaussian.predict(X_test)
+
+print(gaussian.score(X_train, Y_train))
