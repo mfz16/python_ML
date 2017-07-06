c4b77d633fd34a5ffb72a7fcd4332ef852442b7e
faraz <mfz16>
2017-07-06 21:08:48 +0530

    update1

---
+++ sikuli.py
@@ -0,0 +1,16 @@
+import sikuli
+import time
+
+click("1484930000836.png")
+type("hello")
+time.sleep(2)
+click("1484930000836.png")
+type("how are you")
+time.sleep(2)
+click("1484930000836.png")
+type("lllll")
+i = 0
+while (i < 10):
+    click("1484930441754.png")
+    i = i + 1
+
